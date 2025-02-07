2ff4f7bf233bab214954a0a18288a051bd05f379
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2025-02-07 10:28:57 +0530

    @PostConstruction and @PreDestruction

--- com.practice/src/main/java/com/practice/spring-basics/src/main/java/com/practice/springBasics/SpringBasicsApplication.java
+++ com.practice/src/main/java/com/practice/spring-basics/src/main/java/com/practice/springBasics/SpringBasicsApplication.java
@@ -1,11 +1,35 @@
 package com.practice.springBasics;
 
 import com.practice.springBasics.BusinessCalculationService.BusinessCalculationServiceRunner;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+
+@Component
+class PostConstruction{
+	PostConstruction(){
+		System.out.println("post Construction has been initialized");
+	}
+	@PostConstruct
+	void construct(){
+		System.out.println("Post construct methods has been called");
+	}
+}
+@Component
+class PreDestruction{
+	PreDestruction(){
+		System.out.println("pre destruction has been initialized");
+	}
+	@PreDestroy
+	void destroy(){
+		System.out.println("destruction method has been called");
+	}
+}
 
 @Configuration
 @ComponentScan
@@ -13,9 +37,7 @@ public class SpringBasicsApplication {
 
 	public static void main(String[] args) {
 try(var context= new AnnotationConfigApplicationContext(SpringBasicsApplication.class)){
-	Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
-	BusinessCalculationServiceRunner object= context.getBean(BusinessCalculationServiceRunner.class);
-	System.out.println(object.findMax());
+	context.getBeanDefinitionNames();
 }
 
 
