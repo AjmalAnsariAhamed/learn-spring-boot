417667597162528d1902b57ff1daa145725ea5a9
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2025-02-10 16:30:46 +0530

    learning spring-boot

---
+++ com.practice/src/main/java/com/practice/learn-spring-boot/src/main/java/com/practice/learn_spring_boot/CourseController/CourseController.java
@@ -0,0 +1,31 @@
+package com.practice.learn_spring_boot.CourseController;
+
+import com.practice.learn_spring_boot.Entity.Courses;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+    private List<Courses>courses= new ArrayList<>();
+    @GetMapping("/health-check")
+    public String healthCheck(){
+
+        return "all Okay";
+    }
+    @GetMapping(path = "/courses")
+    public List<Courses> getAllCourses(){
+        courses.addAll(Arrays.asList(new Courses(1,"Dragon ball z","Akira toriyama"),
+                new Courses(2,"Naruto","Masashi Kishimoto"),new Courses(3,"OnePiece","Eiichiro Oda")));
+        return courses;
+    }
+    @PostMapping("/add")
+    public void post(@RequestBody Courses course) {
+        courses.add(course);
+        System.out.println(courses);
+
+    }
+}
