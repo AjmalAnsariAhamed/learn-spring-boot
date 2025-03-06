5c8c376a2c38b353c909c2da14e34b851fcc8fe3
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2025-03-06 12:59:25 +0530

    Basic Authentication-wth-httpBasic

---
+++ com.practice/src/main/java/com/practice/spring-security-6/src/main/java/com/practice/spring_security_6/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.practice.spring_security_6.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(
+                req -> req.anyRequest().authenticated()
+        ).httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
+
+    }
+
+}
