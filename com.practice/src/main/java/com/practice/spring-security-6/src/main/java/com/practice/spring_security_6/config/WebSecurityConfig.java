7054c58be5e8bd1342a670508428407afc0551b1
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2025-03-06 13:20:56 +0530

    Multiple users-auth with the InMemoryUserDetailsManager

--- com.practice/src/main/java/com/practice/spring-security-6/src/main/java/com/practice/spring_security_6/config/WebSecurityConfig.java
+++ com.practice/src/main/java/com/practice/spring-security-6/src/main/java/com/practice/spring_security_6/config/WebSecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,6 +22,18 @@ public class WebSecurityConfig {
 
         return httpSecurity.build();
 
+    }
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails luffy= User.withUsername("luffy")
+                .password("pasword")
+                .roles("USER")
+                .build();
+        UserDetails zoro= User.withUsername("zoro")
+                .password("three-swords")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(luffy,zoro);
     }
 
 }
